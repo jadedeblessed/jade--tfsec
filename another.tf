0d70eb4145365427e8346a739cd696d0e7a0eb82
Jadesdeblessed <jadesolaemmanuel@gmail.com>
2023-07-14 15:59:17 -0400

    ist git action

--- another.tf
+++ another.tf
@@ -89,3 +89,23 @@ server_side_encryption_configuration {
 
 }
 
+resource "aws_s3_bucket_public_access_block" "my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id
+  block_public_acls = true
+}
+resource "aws_s3_bucket_public_access_block" "my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id 
+  block_public_policy = true 
+}
+
+ resource "aws_s3_bucket_public_access_block" "my_bucket" {
+    bucket = aws_s3_bucket.my_bucket.id
+
+    ignore_public_acls = true
+ }
+
+  resource "aws_s3_bucket_public_access_block" "my_bucket" {
+    bucket = aws_s3_bucket.my_bucket.id
+    block_public_acls   = true
+    block_public_policy = true
+ }
