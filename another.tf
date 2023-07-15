d5b04e10306628b8619b0892e178773bdee95b28
Jadesdeblessed <jadesolaemmanuel@gmail.com>
2023-07-15 02:18:28 -0400

    ist git action

--- another.tf
+++ another.tf
@@ -17,7 +17,10 @@ resource "aws_instance" "web" {
 resource "aws_vpc" "demo_vpc" {
   cidr_block = "10.0.0.0/16"
 }
-
+resource "aws_flow_log" "demo_vpc" {
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.demo_vpc.id
+}
 resource "aws_autoscaling_group" "my_asg" {
   availability_zones        = ["us-west-1a"]
   name                      = "my_asg"
